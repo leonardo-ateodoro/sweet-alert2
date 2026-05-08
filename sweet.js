8c4796d1ef6f57a6ac34ebb6c730233f3ae297a6
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-05-08 14:58:00 -0300

    metade do js

--- sweet.js
+++ sweet.js
@@ -17,7 +17,40 @@ document.getElementById('alertBasico').addEventListener('click', function() {
     document.getElementById('alertInfo').addEventListener('click', function() {
         Swal.fire('Informação', 'Este é um alerta de informação.','info');   
     });
-~~~~~
 
 
-    
+    document.getElementById('alertConfirm').addEventListener('click', function() {
+        Swal.fire ({
+            title: 'Você tem certeza?',
+            text: "Esta ação é irreversível!",
+            icon:'question',
+            showCancelButton: true,
+            confirmButtonText: 'Sim,prosseguir!',
+            cancelButtonText:'Cancelar'        
+        
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('Confirmado!', 'Você confirmou a ação. ','success');
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                Swal.fire('Cancelado', 'A ação foi cancelada', 'error');
+            }
+        }); 
+    });
+    document.getElementById('alertInput').addEventListener('click',function() {
+        Swal.fire({
+            title: 'Digite seu nome',
+            input:'text',
+            inputPlaceholder:'Seu nome aqui',
+            showCancelButton: true,
+            confirmButtonText:'Enviar',
+            cancelButtonText:'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed && result.value) {
+                Swal.fire(`Olá, ${result.value}!`,'Seu nome foi recebido.','sucess');
+            }
+        });
+    });
+
+    document.getElementById('alertTimer').addEventListener('click', function(){
+        
+    })
