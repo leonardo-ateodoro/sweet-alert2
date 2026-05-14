a699cd46dc8ae0c597d59e325367e6a69e09802d
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-05-14 10:37:37 -0300

    TERMINEI O JS

--- sweet.js
+++ sweet.js
@@ -52,5 +52,49 @@ document.getElementById('alertBasico').addEventListener('click', function() {
     });
 
     document.getElementById('alertTimer').addEventListener('click', function(){
+        swal.fire({
+            title: 'Auto Fechamento!',
+            text: 'Este alerta será fechado em 3 segundos.',
+            timer: 3000,
+            timerProgressBar: true
+        });    
+    });
+
+    
+    document.getElementById('alertTimerCount').addEventListener('click', function () {
+        let timerInterval;
+        Swal.fire ({
+            title: "Auto Fechamento",
+            html: "Esse alerta será fechado em <strong></strong> segundos.",
+            timer: 5000,
+            timerProgressBar: true, 
+            didOpen: () => {
+                Swal.SHOWloading();
+                const timer = Swal.getPopup ().querySelector ('strong');
+                timerInterval = setInterval (() => {
+                    timer.textContent = `${(swal.getTimerLeft()/1000).toFixed(0)}`;
+                },100);  
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            }
+        });
+     });
+    
+
+    document.getElementById('alertHtml').addEventListener('click', function(){
+        Swal.fire ({
+            title: '<strong>Alerta com HTML </strong>',
+            icon : 'info',
+            html : 'Você pode usar <strong> HTML customizado </strong> aqui! <br> <a href = "https://sweetalert2.github.io" target= "_blank" >Mais informações </a>.',
+            showCloseButton: true,
+            focusConfirm: false,
+            confirmButtonText:'OK',
+            color: 'red',
+            position: 'top-end'
+
+        });
         
-    })
+    });
+        
+    
